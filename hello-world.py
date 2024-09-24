7b42a0c906e575f5ff3061a1f54b4f76e3e1f641
jmar <jmar199813@gmail.com>
2024-09-24 01:43:31 -0600

    Used a format() function

--- hello-world.py
+++ hello-world.py
@@ -11,3 +11,9 @@ print("Hello, " + "World" + "!" + " 3")
 #f string
 name = "World"
 print(f"Hello, {name}! 4")
+
+#format 
+name1 = "Hello"
+name2 = "World"
+formatted_string = "{}, {}! 5".format(name1, name2)
+print(formatted_string)
