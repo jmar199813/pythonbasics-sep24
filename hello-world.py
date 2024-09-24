65429d6c9ab310cbdec65bbabf289a93988e34ac
jmar <jmar199813@gmail.com>
2024-09-24 02:22:17 -0600

    Learning the syntax of loops and overall Python is pretty nice and relatively simple
    The * half pyramid took a lot less writing than it did in C++

--- hello-world.py
+++ hello-world.py
@@ -23,6 +23,55 @@ for phrase in ["Hello, World! 6"]:
   print(phrase)
 
 #loop2
-slogans = ["Hello", "World", "!"]
+slogans = ["Hello", "World", "!", "#7"]
 for slogan in slogans:
   print(slogan)
+
+#range loop
+for i in range(4):
+  print(i)
+
+#while loop
+count = 0
+while count < 5:
+  print(count)
+  count += 1
+
+#skips over the number 2
+for i in range(7):
+  if i == 2:
+    continue
+  print(i)  
+
+#nested loops  
+for i in range(3):
+  for j in range(2):
+    print(f"i: {i}, j: {j}")
+
+#nested loop string example
+colours = ["red", "green", "blue"]
+numbers = ["1", "2", "3"]
+
+for colour in colours:
+  for number in numbers:
+    print(f"{colour} {number}")
+
+#nested loop pattern
+for i in range(7): # rows
+  for j in range(i + 1): # columns
+    print("*", end="")
+  print() # moves to a new line after each row
+
+#nested loop patter backwards
+rows = 7
+
+for i in range(rows, 0, -1):
+  for j in range(i):
+    print("*", end="")
+  print()
+
+#time for a new Hello, Woorld so I can remeber how I got here
+print("Hello, World!")
+print("Hello, World!")
+print("Hello, World!")
+  
