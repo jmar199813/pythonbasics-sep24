cac454620943d1db62ea12994a795e2894b18ead
jmar <jmar199813@gmail.com>
2024-09-24 01:36:00 -0600

    Printing Hello, World using a variable, string conc, f string.

--- hello-world.py
+++ hello-world.py
@@ -1,1 +1,13 @@
-print("Hello, World!")
+#simple
+print("Hello, World! 1")
+
+#Using a variable
+message = "Hello, World! 2"
+print(message)
+
+#string concatenation
+print("Hello, " + "World" + "!" + " 3")
+
+#f string
+name = "World"
+print(f"Hello, {name}! 4")
